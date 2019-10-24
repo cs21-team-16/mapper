b9d5c1c55a5bcb4e68b5a08db1c4c6f0f06991fe
Robert Driskell <driskell.ba@gmail.com>
2019-10-23 20:57:21 -0500

    Getting gold, selling at shop, I'M RICH

--- gold_getter_goblins.py
+++ gold_getter_goblins.py
@@ -1,6 +1,7 @@
 import json
 import time
 import random
+import requests
 from decouple import config
 
 # TEST_URL = "http://localhost:8000/api/adv"
@@ -19,15 +20,17 @@ print(f"first room: {map[0]}")
 def look_for_shop(gold):
     queue = []
     visited = set()
-    init = requests.get(url = f"{BASE_url}/init/",  headers = HEADERS)
+    init = requests.get(url = f"{BASE_URL}/init/",  headers = HEADERS)
     init_res = json.loads(init.text)
     time.sleep(init_res["cooldown"])
     current_room = init_res['room_id']
     queue.append([map[current_room]])
     while len(queue) > 0:
-        path = queue.pop(0)
+        path = queue.pop()
         vertex = path[-1]
         if vertex["room_id"] not in visited:
+            print(vertex["title"])
+            visited.add(vertex["room_id"])
             if vertex["title"] == "Shop":
                 directions = []
                 for i in range(0, len(path)-1):
@@ -39,30 +42,30 @@ def look_for_shop(gold):
                     res = json.loads(r.text)
                     print(res)
                     time.sleep(res["cooldown"])
-                    break
-            visited.add(vertex["room_id"])
+                break
             for i in vertex["exits"]:
-                if vertex["exits"][i]:
+                if vertex["exits"][i] is not None and vertex["exits"][i] not in visited:
                     new_path = list(path)
                     new_path.append(map[vertex["exits"][i]])
+                    queue.insert(0, new_path)
     r = requests.post(url = f"{BASE_URL}/status/", headers = HEADERS)
     res = json.loads(r.text)
     print(res)
     time.sleep(res["cooldown"])
     if len(res["inventory"]) > 0:
-        for i in res["inventory"]:
+        for i in range(0, len(res["inventory"])):
             # if "treasure" in res["inventory"][i]:
-            if "treasure" in i:
+            if "treasure" in res["inventory"][i]:
                 req = requests.post(url = f"{BASE_URL}/sell/", headers = HEADERS, json = {"name": res["inventory"][i]})
                 result = json.loads(req.text)
-                print(result)
+                print(f"LOOK HERE: {result}")
                 time.sleep(result["cooldown"])
 
                 req = requests.post(url = f"{BASE_URL}/sell/", headers = HEADERS, json = {"name": res["inventory"][i], "confirm":"yes"})
                 result = json.loads(req.text)
                 # global gold
                 # gold += #Some value here that 
-                print(result)
+                print(f"LOOK HERE: {result}")
                 time.sleep(result["cooldown"])
                 # This might need to be edited for confirmation of sale
     
@@ -81,7 +84,7 @@ def look_for_treasure():
         "e": "w",
         "w": "e"
     }
-    init = requests.get(url = f"{BASE_url}/init/",  headers = HEADERS)
+    init = requests.get(url = f"{BASE_URL}/init/",  headers = HEADERS)
     init_res = json.loads(init.text)
     time.sleep(init_res["cooldown"])
     current_room_ID = init_res['room_id']
@@ -98,10 +101,12 @@ def look_for_treasure():
     while ready_to_sell is False:
         directions = []
         for i in map[current_room_ID]["exits"]:
-            if i is not opposites[last_move]:
-                directions.append(i)
+            if map[current_room_ID]["exits"][i]:
+                if last_move is None or i not in opposites[last_move]:
+                    directions.append(i)
         if len(directions) == 0:
             break
+        print(directions)
         choice = random.randrange(0,len(directions))
         last_move = directions[choice]
         if len(current_room["items"]) > 0:
@@ -109,19 +114,25 @@ def look_for_treasure():
                 if "treasure" in i:
                     item = requests.post(url = f"{BASE_URL}/examine/", headers = HEADERS, json = {"name":i})
                     item_res = json.loads(item.text)
-                    weight = item_res["weight or size or something"]
+                    print(item_res)
+                    weight = item_res["weight"]
                     time.sleep(item_res["cooldown"])
 
                     if weight <= strength - encumbrance:
                         encumbrance += weight
-                        requests.post(url = f"{BASE_URL}/take/", headers = HEADERS, json = {"name": i})
+                        grab = requests.post(url = f"{BASE_URL}/take/", headers = HEADERS, json = {"name": i})
+                        grab_res = json.loads(grab.text)
+                        time.sleep(grab_res["cooldown"])
                         if encumbrance == strength:
                             ready_to_sell = True
                             break
                     else:
                         ready_to_sell = True
-        r = requests.post(url = f"{BASE_URL}/move/", headers = HEADERS, json = {"direction": last_move})
+        r = requests.post(url = f"{BASE_URL}/move/", headers = HEADERS, json = {"direction": last_move, "next_room_id": f"{map[current_room_ID]['exits'][last_move]}"})
         r_res = json.loads(r.text)
+        print(r_res)
+        current_room = r_res
+        current_room_ID = r_res["room_id"]
         time.sleep(r_res["cooldown"])
                 
             
@@ -132,7 +143,7 @@ def change_name():
     # if gold >= 1000 stop looking for treasure, find name changer, change name
     queue = []
     visited = set()
-    init = requests.get(url = f"{BASE_url}/init/",  headers = HEADERS)
+    init = requests.get(url = f"{BASE_URL}/init/",  headers = HEADERS)
     init_res = json.loads(init.text)
     time.sleep(init_res["cooldown"])
     current_room = init_res['room_id']
@@ -175,11 +186,11 @@ def change_name():
 
 
 
-stats = requests.get(url = f"{BASE_url}/status/",  headers = HEADERS)
+stats = requests.post(url = f"{BASE_URL}/status/",  headers = HEADERS)
 stats_res = json.loads(stats.text)
 gold = stats_res["gold"]
 time.sleep(stats_res["cooldown"])
-while gold < 0:
-    look_for_treasure()
+while gold < 1000:
+    # look_for_treasure()
     look_for_shop(gold)
 change_name()
